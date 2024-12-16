78fb27832f49d7445b158f59309abb9af209b98a
Y8410T <yasutotianzhong354@gmail.com>
2024-12-17 02:45:41 +0900

    一人当たりの平均使用金額の追加

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template
 from models import initialize_database
 from routes import blueprints
+from models import Customer, Reservation
+from peewee import fn
 
 app = Flask(__name__)
 
@@ -14,7 +16,14 @@ for blueprint in blueprints:
 # ホームページのルート
 @app.route('/')
 def index():
-    return render_template('index.html')
+    total_num_people = int(Customer.select(fn.SUM(Customer.numPeople)).scalar() or 0)
+    total_sum_price = sum([reservation.food.price + reservation.drink.price for reservation in Reservation.select()])
+    if total_num_people > 0:
+        average_price_per_person = total_sum_price / total_num_people
+    else:
+        average_price_per_person = 0
+    return render_template('index.html', total_num_people=total_num_people, average_price_per_person=average_price_per_person)
+    
 
 if __name__ == '__main__':
     app.run(port=8080, debug=True)
